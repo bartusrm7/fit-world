395e47542be4c5f792ea6dfe054d6dc11fd2ccd1
Bartek <bbogucki7@gmail.com>
2024-03-29 10:40:25 +0100

    connect adding calories between windows

--- fit-world-frontend/src/MainContainer.jsx
+++ fit-world-frontend/src/MainContainer.jsx
@@ -16,13 +16,18 @@ function MainContainer() {
 				throw new Error("Network is not working!");
 			}
 			const data = await response.json();
-			console.log(data);
-			setFood([...food, data]);
+			console.log(data[0]);
+			setFood([...food, data[0]]);
 			setFoodInput("");
 		} catch (error) {
 			console.error("Error searching the food:", error);
 		}
 	};
+	function useEnterKey(e) {
+		if (e.key === "Enter") {
+			addFood();
+		}
+	}
 	function handleFoodInputChange(e) {
 		setFoodInput(e.target.value);
 	}
@@ -41,8 +46,14 @@ function MainContainer() {
 			<div className='main-window'>
 				<div className='main-grid-container'>
 					<div className='main-grid-items item1'>
-						<span>Add the food:</span>
-						<input type='text' className='input-food' value={foodInput} onChange={handleFoodInputChange} />
+						<span className='label-of-main-window-parts'>Add the food:</span>
+						<input
+							type='text'
+							className='input-food'
+							value={foodInput}
+							onChange={handleFoodInputChange}
+							onKeyDown={useEnterKey}
+						/>
 						<button className='add-food-btn' onClick={addFood}>
 							+
 						</button>
@@ -52,23 +63,98 @@ function MainContainer() {
 							))}
 						</ul>
 					</div>
-					<div className='main-grid-items item2'>Total Calories</div>
-					<div className='main-grid-items item3'>
-						Today Calories <span className='current-calories-day'>{currentDate()}</span>
+					<div className='main-grid-items item2'>
+						<span className='label-of-main-window-parts'>Total Calories</span>
+						<div>
+							<p>
+								<span className='total-calories-per-day'>2500</span>kcal
+							</p>
+						</div>
 					</div>
-					<div className='main-grid-items item4'>Measurements</div>
-					<div className='main-grid-items item5'>Favorites Meal</div>
-					<div className='main-grid-items item6'>Burned Calories</div>
+					<div className='main-grid-items item3'>
+						<div className='label-and-date-container-item3'>
+							<span className='label-of-main-window-parts'>Today Calories</span>
+							<span className='current-calories-day'>{currentDate()}</span>
+						</div>
+						<div className='circle-calories-container'>
+							<div className='outer'>
+								<div className='inner'></div>
+							</div>
+						</div>
+						<div className='macronutrients-container'>
+							<p>
+								P
+								<input type='checkbox' />
+							</p>
+							<p>
+								C<input type='checkbox' />
+							</p>
+							<p>
+								F<input type='checkbox' />
+							</p>
+						</div>
+					</div>
+					<div className='main-grid-items item4'>
+						<span className='label-of-main-window-parts'>Measurements</span>
+					</div>
+					<div className='main-grid-items item5'>
+						<span className='label-of-main-window-parts'>Favorites Meal</span>
+					</div>
+					<div className='main-grid-items item6'>
+						<span className='label-of-main-window-parts'>Burned Calories</span>
+					</div>
 					<div className='main-grid-items item7'>
-						Weekdays
+						<span className='label-of-main-window-parts'>Weekdays</span>
 						<div className='days'>
-							<p className='day'>Mon</p>
-							<p className='day'>Tue</p>
-							<p className='day'>Wedy</p>
-							<p className='day'>Thu</p>
-							<p className='day'>Fri</p>
-							<p className='day'>Sat</p>
-							<p className='day'>Sun</p>
+							<p className='day'>
+								{" "}
+								<span className='progress-day'>
+									<input type='checkbox' />
+								</span>
+								Mon
+							</p>
+							<p className='day'>
+								{" "}
+								<span className='progress-day'>
+									<input type='checkbox' />
+								</span>
+								Tue
+							</p>
+							<p className='day'>
+								{" "}
+								<span className='progress-day'>
+									<input type='checkbox' />
+								</span>
+								Wed
+							</p>
+							<p className='day'>
+								{" "}
+								<span className='progress-day'>
+									<input type='checkbox' />
+								</span>
+								Thu
+							</p>
+							<p className='day'>
+								{" "}
+								<span className='progress-day'>
+									<input type='checkbox' />
+								</span>
+								Fri
+							</p>
+							<p className='day'>
+								{" "}
+								<span className='progress-day'>
+									<input type='checkbox' />
+								</span>
+								Sat
+							</p>
+							<p className='day'>
+								{" "}
+								<span className='progress-day'>
+									<input type='checkbox' />
+								</span>
+								Sun
+							</p>
 						</div>
 					</div>
 				</div>
